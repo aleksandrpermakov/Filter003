1bd8bd8dc8f260fbdc62d9d5f8b5aa804f22ff73
aleksandrpermakov <aleksandrpermakov105@gmail.com>
2025-05-28 23:19:01 +0500

    Добавьте файлы проекта.

---
+++ Filter003/MyFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter003
+{
+    internal class MyFilter
+    {
+        public int AmountOfArray(int[] array)
+        {
+            int result = 0;
+            foreach (var item in array)
+            {
+                if (item > -10 && item > 10)
+                {
+                    result += item;
+                }
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ Filter003/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFilter summ = new MyFilter();
+            int[] arr = { -20, -5, 6, 15, 10 };
+            summ.AmountOfArray(arr);
+
+        }
+    }
+}
